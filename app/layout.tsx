8b4fd6dbad3f03fcb7760fa5c33d05dedc29c1b2
Pawel-Szydlo <pawel.szydlo.99@gmail.com>
2024-03-20 12:20:49 +0100

    check name image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,39 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Head from 'next/head'
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Aptos Template App',
+  title: "Aptos Template App",
   description:
-    'Start your Aptos journey here, without unnecessary configuration and setup. Just clone it and code on top of it. Powered by Nightly Wallet.',
+    "Start your Aptos journey here, without unnecessary configuration and setup. Just clone it and code on top of it. Powered by Nightly Wallet.",
   twitter: {
-    title: 'Aptos Template App',
+    title: "Aptos Template App",
     description:
-      'Start your Aptos journey here, without unnecessary configuration and setup. Just clone it and code on top of it. Powered by Nightly Wallet.',
-    images: 'https://aptos-web3-template.nightly.app/preview.png',
-    card: 'summary_large_image',
-    site: '@nightly_app',
+      "Start your Aptos journey here, without unnecessary configuration and setup. Just clone it and code on top of it. Powered by Nightly Wallet.",
+    images: "https://aptos-web3-template.nightly.app/preview_aptos.png",
+    card: "summary_large_image",
+    site: "@nightly_app",
   },
   openGraph: {
-    title: 'Aptos Template App',
+    title: "Aptos Template App",
     description:
-      'Start your Aptos journey here, without unnecessary configuration and setup. Just clone it and code on top of it. Powered by Nightly Wallet.',
-    images: 'https://aptos-web3-template.nightly.app/preview.png',
-    url: 'https://aptos-web3-template.nightly.app',
-    type: 'website',
+      "Start your Aptos journey here, without unnecessary configuration and setup. Just clone it and code on top of it. Powered by Nightly Wallet.",
+    images: "https://aptos-web3-template.nightly.app/preview_aptos.png",
+    url: "https://aptos-web3-template.nightly.app",
+    type: "website",
   },
-}
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }

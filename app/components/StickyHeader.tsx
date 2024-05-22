1a8e8fc56f04b2a1ced42eaf69db728ab7dabdab
Pawel-Szydlo <pawel.szydlo.99@gmail.com>
2024-05-22 13:56:18 +0200

    test

--- app/components/StickyHeader.tsx
+++ app/components/StickyHeader.tsx
@@ -4,10 +4,24 @@ import {
   AccountAddress,
   AptosApiType,
   Ed25519PrivateKey,
+  Ed25519Signature,
   get,
   MoveVector,
   RotationProofChallenge,
   U8,
+  generateRawTransaction,
+  generateTransactionPayloadWithABI,
+  TypeTagU8,
+  TypeTagVector,
+  EntryFunctionABI,
+  AnyRawTransaction,
+  AnyTransactionPayloadInstance,
+  AptosConfig,
+  InputGenerateMultiAgentRawTransactionData,
+  InputGenerateTransactionData,
+  InputGenerateTransactionPayloadDataWithRemoteABI,
+  buildTransaction,
+  generateTransactionPayload,
 } from "@aptos-labs/ts-sdk";
 import { AccountInfo, UserResponseStatus } from "@aptos-labs/wallet-standard";
 import React, { useEffect } from "react";
@@ -16,6 +30,91 @@ import { getAdapter } from "../misc/adapter";
 import { getAptos } from "../misc/aptos";
 import ActionStarryButton from "./ActionStarryButton";
 import StarryButton from "./StarryButton";
+
+export async function generateTransaction(
+  args: { aptosConfig: AptosConfig } & InputGenerateTransactionData
+): Promise<AnyRawTransaction> {
+  const payload = await buildTransactionPayload(args);
+  return buildRawTransaction(args, payload);
+}
+
+export async function buildTransactionPayload(
+  args: { aptosConfig: AptosConfig } & InputGenerateTransactionData
+): Promise<AnyTransactionPayloadInstance> {
+  const { aptosConfig, data } = args;
+  // Merge in aptosConfig for remote ABI on non-script payloads
+  let generateTransactionPayloadData: InputGenerateTransactionPayloadDataWithRemoteABI;
+  let payload: AnyTransactionPayloadInstance;
+
+  if ("bytecode" in data) {
+    // TODO: Add ABI checking later
+    payload = await generateTransactionPayload(data);
+  } else if ("multisigAddress" in data) {
+    generateTransactionPayloadData = {
+      aptosConfig,
+      multisigAddress: data.multisigAddress,
+      function: data.function,
+      functionArguments: data.functionArguments,
+      typeArguments: data.typeArguments,
+      abi: data.abi,
+    };
+    payload = await generateTransactionPayload(generateTransactionPayloadData);
+  } else {
+    generateTransactionPayloadData = {
+      aptosConfig,
+      function: data.function,
+      functionArguments: data.functionArguments,
+      typeArguments: data.typeArguments,
+      abi: data.abi,
+    };
+    payload = await generateTransactionPayload(generateTransactionPayloadData);
+  }
+  return payload;
+}
+
+export async function buildRawTransaction(
+  args: { aptosConfig: AptosConfig } & InputGenerateTransactionData,
+  payload: AnyTransactionPayloadInstance
+): Promise<AnyRawTransaction> {
+  const { aptosConfig, sender, options } = args;
+
+  let feePayerAddress;
+  if (isFeePayerTransactionInput(args)) {
+    feePayerAddress = AccountAddress.ZERO.toString();
+  }
+
+  if (isMultiAgentTransactionInput(args)) {
+    const { secondarySignerAddresses } = args;
+    return buildTransaction({
+      aptosConfig,
+      sender,
+      payload,
+      options,
+      secondarySignerAddresses,
+      feePayerAddress,
+    });
+  }
+
+  return buildTransaction({
+    aptosConfig,
+    sender,
+    payload,
+    options,
+    feePayerAddress,
+  });
+}
+
+function isFeePayerTransactionInput(
+  data: InputGenerateTransactionData
+): boolean {
+  return data.withFeePayer === true;
+}
+
+function isMultiAgentTransactionInput(
+  data: InputGenerateTransactionData
+): data is InputGenerateMultiAgentRawTransactionData {
+  return "secondarySignerAddresses" in data;
+}
 
 const StickyHeader: React.FC = () => {
   const [userAccount, setUserAccount] = React.useState<
@@ -198,7 +297,9 @@ const StickyHeader: React.FC = () => {
 
                     const newAccount = Account.fromPrivateKey({
                       privateKey: new Ed25519PrivateKey(
-                        "25be971b8ef7fb9e1f8ff4a0acf6d96fb1ae1d8acc7e9b027c31f74d50475cbd"
+                        // "25be971b8ef7fb9e1f8ff4a0acf6d96fb1ae1d8acc7e9b027c31f74d50475cbd"
+                        // "d61ed94ca68af2ddc29c3192e8212636a80c1db369fa7a732428baeb0860c4c0"
+                        "f024c1f864680aaef1117692c7a6567767975f09060a2d562884078f7b86d43a"
                       ),
                       legacy: true,
                     });
@@ -210,43 +311,82 @@ const StickyHeader: React.FC = () => {
                       path: `accounts/${userAccount.address.toString()}`,
                     })) as any;
 
-                    const address = AccountAddress.fromString(
-                      userAccount.publicKey.toString()
-                    );
-
                     const challenge = new RotationProofChallenge({
                       sequenceNumber: BigInt(accountInfo.sequence_number),
-                      originator: address,
+                      originator: userAccount.address,
                       currentAuthKey: AccountAddress.from(
                         accountInfo.authentication_key
                       ),
                       newPublicKey: newAccount.publicKey,
                     });
-
+                    // console.log(
+                    //   newAccount,
+                    //   newAccount.signingScheme,
+                    //   newAccount.publicKey.toString()
+                    // );
                     const challengeHex = challenge.bcsToBytes();
                     const textDecoder = new TextDecoder();
-                    const string = textDecoder.decode(challengeHex);
+                    const challengeHexString = textDecoder.decode(challengeHex);
 
                     const data = await adapter.signMessage({
-                      message: string,
-                      address: true,
-                      nonce: "change access",
+                      message: challengeHexString,
+                      nonce: accountInfo.sequence_number,
                     });
+                    console.log(data);
 
                     // missing logic
                     const proofSignedByCurrentPrivateKey: any =
                       //@ts-expect-error
                       data.args.signature;
 
-                    const proofSignedByNewPrivateKey =
-                      newAccount.sign(challengeHex);
+                    // const newSignature = new Ed25519Signature(
+                    //   //@ts-expect-error
+                    //   data.args.signature.data.data
+                    // );
+                    //@ts-expect-error
+                    console.log("po", data.args.signature);
 
-                    const transaction = await aptos.transaction.build.simple({
+                    const proofSignedByNewPrivateKey =
+                      newAccount.sign(challengeHexString);
+
+                    const rotateAuthKeyAbi: EntryFunctionABI = {
+                      typeParameters: [],
+                      parameters: [
+                        new TypeTagU8(),
+                        TypeTagVector.u8(),
+                        new TypeTagU8(),
+                        TypeTagVector.u8(),
+                        TypeTagVector.u8(),
+                        TypeTagVector.u8(),
+                      ],
+                    };
+                    console.log("przed", userAccount!.address.toString());
+                    // const transaction = await aptos.transaction.build.simple({
+                    //   sender: userAccount!.address.toString(),
+                    //   data: {
+                    //     function: "0x1::account::rotate_authentication_key",
+                    //     typeArguments: [],
+
+                    //     functionArguments: [
+                    //       new U8(0), // from scheme
+                    //       MoveVector.U8(userAccount.publicKey.toUint8Array()),
+                    //       new U8(newAccount.signingScheme), // to scheme
+                    //       MoveVector.U8(newAccount.publicKey.toUint8Array()),
+                    //       MoveVector.U8(
+                    //         proofSignedByCurrentPrivateKey.toUint8Array()
+                    //       ),
+                    //       MoveVector.U8(
+                    //         proofSignedByNewPrivateKey.toUint8Array()
+                    //       ),
+                    //     ],
+                    //     abi: rotateAuthKeyAbi,
+                    //   },
+                    // });
+                    const transaction = await generateTransaction({
+                      aptosConfig: aptos.config,
                       sender: userAccount!.address.toString(),
                       data: {
                         function: "0x1::account::rotate_authentication_key",
-                        typeArguments: [],
-
                         functionArguments: [
                           new U8(0), // from scheme
                           MoveVector.U8(userAccount.publicKey.toUint8Array()),
@@ -259,13 +399,13 @@ const StickyHeader: React.FC = () => {
                             proofSignedByNewPrivateKey.toUint8Array()
                           ),
                         ],
+                        abi: rotateAuthKeyAbi,
                       },
                     });
 
-                    //@ts-expect-error rawTransaction
-                    const signedTx = await adapter.signAndSubmitTransaction({
-                      rawTransaction: transaction.rawTransaction,
-                    });
+                    const signedTx = await adapter.signAndSubmitTransaction(
+                      transaction
+                    );
 
                     if (signedTx.status !== UserResponseStatus.APPROVED) {
                       throw new Error("Transaction rejected");
